1a5f663f733e24d396644bb4a7225e13e537faae
tlw52836 <1660872260@qq.com>
2022-01-19 17:59:35 +0800

    Initial commit

---
+++ src/main/java/com/tlw/SpringbootLenrnApplication.java
@@ -0,0 +1,13 @@
+package com.tlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLenrnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLenrnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tlw/SpringbootLenrnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLenrnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
